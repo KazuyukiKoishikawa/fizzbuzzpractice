8d980f4961d6bac882ae2c793f07ee6bfb8ca083
koi <kazuyuki.koishikawa@jp.yokogawa.com>
2022-06-07 15:49:08 +0900

    first commit

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Say(int number)
+        {
+            return "1";
+        }
+    }
+}
---
+++ FizzBuzzTests/FizzBuzzTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestClass()]
+    public class FizzBuzzTests
+    {
+        [TestMethod()]
+        public void SayGiven1Return1()
+        {
+            var fz = new FizzBuzz();
+            Assert.AreEqual("1", fz.Say(1));
+        }
+    }
+}
